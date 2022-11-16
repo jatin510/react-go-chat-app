f3f119895c1da25b09ce397dce12af81ea0ccbb6
jatin510 <jatin6972@gmail.com>
2022-11-16 15:38:54 +0530

    added websocket handler

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("error in ws reader", err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Host)
+
+	// upgrade to ws connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// listen for new messages
+	reader(ws)
+}
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Simple server")
 	})
+
+	// map 'ws' endpoint to `serverWs` function
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
+	log.Println("Chat application")
 	setupRoutes()
 	http.ListenAndServe(":8081", nil)
 }
